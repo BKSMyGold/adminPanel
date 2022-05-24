4967e91a79b994a2648f6f8d2eef9d4cbcf39ac3
adeptcoder014 <gupta.nischal014@gmail.com>
2022-05-24 16:14:39 +0530

    decluttered the Header and the Footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -8,20 +8,20 @@ export class Footer extends Component {
 						<div className="container-xxl d-flex flex-column flex-md-row align-items-center justify-content-between">
 							{/*begin::Copyright*/}
 							<div className="text-dark order-2 order-md-1">
-								<span className="text-muted fw-bold me-1">2021©</span>
-								<a href="https://keenthemes.com" target="_blank" className="text-gray-800 text-hover-primary">BK Saraf</a>
+								<span className="text-muted fw-bold me-1">202©</span>
+								<a href="#" target="_blank" className="text-gray-800 text-hover-primary">BK Saraf</a>
 							</div>
 							{/*end::Copyright*/}
 							{/*begin::Menu*/}
 							<ul className="menu menu-gray-600 menu-hover-primary fw-bold order-1">
 								<li className="menu-item">
-									<a href="https://keenthemes.com" target="_blank" className="menu-link px-2">About</a>
+									<a href="#" target="_blank" className="menu-link px-2">About</a>
 								</li>
 								<li className="menu-item">
-									<a href="https://keenthemes.com/support" target="_blank" className="menu-link px-2">Support</a>
+									<a href="#" target="_blank" className="menu-link px-2">Privacy</a>
 								</li>
 								<li className="menu-item">
-									<a href="https://1.envato.market/EA4JP" target="_blank" className="menu-link px-2">Purchase</a>
+									<a href="#" target="_blank" className="menu-link px-2">Policy</a>
 								</li>
 							</ul>
 							{/*end::Menu*/}
