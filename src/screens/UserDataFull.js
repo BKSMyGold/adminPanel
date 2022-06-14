b2a0dde0b6d9701be7b14c9622e8c52715e8107a
adeptcoder014 <gupta.nischal014@gmail.com>
2022-06-14 14:24:08 +0530

    User Full Data Report

--- src/screens/UserDataFull.js
+++ src/screens/UserDataFull.js
@@ -3,134 +3,19 @@ import Footer from '../layouts/Footer'
 import Header from '../layouts/Header'
 import Dashboard from './dashboard'
 import axios from 'axios'
+import { BASE_URL } from '../Constants'
 
 const UserDataFull = () => {
   const [subscriptions, setSubscriptions] = useState([])
 
-  const [plans, setPlans] = useState([])
-  const [Users, setUsers] = useState([])
 
-  useEffect(() => {
-    {
-      /*
-			 /*endpont = /api/subscription/   
-		*/
-    }
+  const [sub, setSub] = useState([])
 
-    const SubscriptionResponse = [].constructor(1).fill({
-      createdAt: '2021-12-13T11:53:44.682Z',
-      customPlan: {
-        cyclePeriod: [
-          {
-            createdAt: '2021-12-10T19:09:32.282Z',
-            cycle: 10,
-            docType: 'CyclePeriod',
-            graceperiod: 240,
-            id: '61b3a5ecea31faacc7b37863',
-            minValue: 5000,
-            minWeight: 1,
-            name: 'Every Month',
-            shortName: 'M',
-            status: 'Active',
-            updatedAt: '2021-12-10T19:09:32.282Z',
-          },
-        ],
-        duration: 24,
-        id: '61b73448b2cb7fdd62a1ea2b',
-        mode: 'Value',
-        name: 'Flexi Plan',
-        planType: 'Flexi',
-      },
-      docType: 'Subscription',
-      id: '61b73448b2cb7fdd62a1ea2c',
-      installments: ['61b73446b2cb7fdd62a1ea2a'],
-      maturityDate: null,
-      planBonus: 0,
-      skipCount: 0,
-      status: 'Running',
-      trackingId: '',
-      unpaidInvestments: 0,
-      unpaidSkips: 0,
-      updatedAt: '2021-12-13T11:53:44.682Z',
-      user: '61b716a57c3e83d0f634da04',
-    })
-
-    setSubscriptions(SubscriptionResponse)
-  }, [])
-
-  useEffect(() => {
-    {
-      /*Use Endpoint to get  user data for each subscription */
-    }
-    {
-      /*endpont = api/user/:userId   */
-    }
-    const userResponse = {
-      GBPBonusEntries: [],
-      GBPcode: '',
-      addresses: [],
-      createdAt: '2021-12-10T19:10:50.435Z',
-      deviceToken:
-        'doHSXifERQGpCQyeHMMij6:APA91bHf9rudMzbqdwv3OjNuiLkI98pDgMKzrsJecfPt0i6F5whpi2d5R1sVleS5Ew823FwiECkEE8oEx6ezFt85NZpshZfcs2PuEpetIzBt5NMZSf7ZZPzefc3qAHixB6YaE4VuxV8x',
-      dob: '2000-11-23',
-      docType: 'User',
-      email: 'ashutoshsenapati2311@gmail.com',
-      fname: 'Ashutosh Senapati',
-      id: '61b3a63aea31faacc7b37864',
-      image:
-        'https://bks-gold.s3.ap-south-1.amazonaws.com/User%2Fimage_picker6967090782453535724.jpg',
-      isInvested: false,
-      isWhatsapp: true,
-      joiningBonus: 0,
-      level: '',
-      mobile: 9777139671,
-      pan: 'DEPPP5884H',
-      refCode: '',
-      referenceType: '',
-      referral: null,
-      referralBonusEntries: [],
-      role: '',
-      updatedAt: '2021-12-14T05:50:11.532Z',
-    }
-
-    setUsers(userResponse)
-  }, [])
-
-  useEffect(() => {
-    {
-      /*
-			 Get all  Plans Avaialable using api/plan/:planid
-		*/
-    }
-
-    const PlanResponse = {
-      bonus: 1,
-      createdAt: '2021-12-10T19:33:23.048Z',
-      cyclePeriod: {
-        createdAt: '2021-12-10T19:09:32.282Z',
-        cycle: 10,
-        docType: 'CyclePeriod',
-        graceperiod: 240,
-        id: '61b3a5ecea31faacc7b37863',
-        minValue: 5000,
-        minWeight: 1,
-        name: 'Every Month',
-        shortName: 'M',
-        status: 'Active',
-        updatedAt: '2021-12-10T19:09:32.282Z',
-      },
-      docType: 'Plan',
-      duration: 24,
-      id: '61b3ab83d59d6bacdd6ef5a3',
-      mode: 'weight',
-      name: '24 month Savings Plan',
-      planType: 'Standard',
-      updatedAt: '2021-12-10T19:33:23.048Z',
-    }
-
-    setPlans(PlanResponse)
-  }, [])
-
+  useEffect(()=>{
+    axios.get(`${BASE_URL}/api/subscription`)
+    .then(res => (setSub(res.data.subscriptions)))
+  },[])
+console.log('Isko Karna Hai =-->',sub)
   return (
     <div className='d-flex flex-column flex-root'>
       <div className='page d-flex flex-row flex-column-fluid'>
@@ -186,16 +71,23 @@ const UserDataFull = () => {
                       </thead>
 
                       <tbody>
-                        {subscriptions.map((subscription) => (
+                        {sub && sub.map((Users) => {
+                           if(Users.plan && Users.plan !== undefined){
+                             return(
                           <tr>
-                            <td>{Users.id}</td>
-                            <td>{Users.fname}</td>
-                            <td>{Users.mobile}</td>
-                            <td>{Users.email}</td>
-                            <td>{Users.pan}</td>
-                            <td>{plans.name}</td>
+                            <td>{Users.user.id}</td>
+                            <td>{Users.user.fname}</td>
+                            <td>{Users.plan.cyclePeriod.name}</td>
+                            <td>{Users.plan.planType}</td>
+                            <td>{Users.plan.mode}</td>
+                            <td>{Users.maturityDate.substring(0,10)}</td>
+                            <td>{Users.status}</td>
+                            <td>{Users.installments.map(x =>(x.gold))} gm</td>
+                            <td>{Users.installments.map(x =>(x.status))}</td>
                           </tr>
-                        ))}
+
+                             )
+                           }})}
                       </tbody>
                       {/*end::Table body*/}
                     </table>
