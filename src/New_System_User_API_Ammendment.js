235a253d7d46ee8b0a006c1d50361e923c69d9ae
adeptcoder014 <gupta.nischal014@gmail.com>
2022-07-01 19:42:53 +0530

    Changes that need to be made in the system-user APIs

---
+++ src/New_System_User_API_Ammendment.js
@@ -0,0 +1,15 @@
+const system_user_POST_APIs = {
+  name: "Surya",
+  email: "surya@email.com",
+  role: "61aced91d253565630a8b971",
+  password: "Surya",
+  phoneNumber: "9876543210",
+  panNumber: "LLL112233",
+  aadharNumber: "123456789876",
+  address: "Hyderabad",
+  upperRole : "",
+  parentRole : "",  // three new responses --------> these helps in creating logics foe upper_role and under_role
+  allowedLogin: 0
+
+};
+
