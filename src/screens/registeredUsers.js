a3ddc7f15c2a0bd47bf1620f15e642680ce8af56
adeptcoder014 <gupta.nischal014@gmail.com>
2022-07-01 19:37:31 +0530

    System-user's under users UI

--- src/screens/registeredUsers.js
+++ src/screens/registeredUsers.js
@@ -2,104 +2,70 @@ import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Header from "../layouts/Header";
-
-// import UserDb from '../db.json'
-
+import Footer from "../layouts/Footer";
+//==================================================================================================
 export default function RegisteredUser() {
+  //==================================================================================================
   const [registeredUser, setRegisteredUser] = React.useState([]);
-
+  //==================================================================================================
   React.useEffect(() => {
     axios.get("https://goldv2.herokuapp.com/api/system-user/").then((res) => {
       console.log(res.data);
       setRegisteredUser(res.data);
     });
   }, []);
-
+  //==================================================================================================
   return (
     <>
       <Header />
       <h1 className="btn btn-primary mt-5">Registered Users are :</h1>
       <table class="table table-row-bordered table-row-black-100 registeredUser">
-        {/*begin::Table head*/}
         <thead>
-          <tr class="fw-bolder">
+          <tr class="fw-bolder text-muted text-center">
             <th class="min-w-150px">Name</th>
             <th class="min-w-140px">Email</th>
-            <th class="min-w-120px">phoneNumber</th>
-            <th class="min-w-120px">panNumber</th>
-            <th class="min-w-100px">aadharNumber</th>
-            <th class="min-w-100px">role</th>
-            <th class="min-w-100px">Action</th>
+            <th class="min-w-120px">Mobile</th>
+            <th class="min-w-120px">PAN</th>
+            <th class="min-w-100px">Aadhaar</th>
+            <th class="min-w-100px">Role</th>
+            <th class="min-w-100px">Parent Role</th>
+            <th class="min-w-100px">Change Name</th>
+            <th class="min-w-100px">Change Role</th>
+
           </tr>
         </thead>
 
         <tbody>
-          {registeredUser.map((user) => (
-            <tr>
-              <td class = "p-4">
-                <a
-                  href="#"
-                  class="text-dark fw-bolder text-hover-primary fs-6"
-                  className="gray"
-                >
-                  {user.name}
-                </a>
-              </td>
-              <td>
-                <a
-                  href="#"
-                  class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6 "
-                >
-                  {user.email}
-                </a>
-              </td>
-              {/* <td>
-                <a
-                  href="#"
-                  class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6"
-                >
-                  {user.password}
-                </a>
-              </td> */}
-              <td class="text-dark fw-bolder text-hover-primary fs-6">
-                {user.phoneNumber}
-              </td>
-              <td>
-                <span class="badge badge-light-warning">{user.panNumber}</span>
-              </td>
-              <td>
-                <span class="badge badge-light-info">{user.aadharNumber}</span>
-              </td>
-              <td>
-                {Object.keys(user).map((obj) => (
-                  <a
-                    href="/master/security/masterUserRights"
-                    class="text-dark fw-bolder text-hover-primary d-block mb-1 fs-6 "
-                  >
-                    <span class="badge badge-light-danger">
-                      {user[obj].role_name}
-                    </span>
-                  </a>
-                ))}
-              </td>
-              <td>
-                <Link
-                 to="/change_role"
-                 state = {user}
-                 >
-                  <button class="btn btn-success">Edit</button>
-                </Link>
-              </td>
-            </tr>
-          ))}
+          {registeredUser.map((user) => {
+            if (user.role) {
+              return (
+                <tr class="text-center fw-bolder">
+                  <td class="p-4">{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phoneNumber}</td>
+                  <td>{user.panNumber}</td>
+                  <td>{user.aadharNumber}</td>
+                  <td>{user.role.role_name}</td>
+                  <td>"Parent Role aayega yaha"</td>
+                  <td>
+                    <Link to="/change_name" state={user}>
+                      <button class="btn btn-success">Edit</button>
+                    </Link>
+                  </td>
+                  <td>
+                    <Link to="/change_role" state={user}>
+                      <button class="btn btn-success">Edit</button>
+                    </Link>
+                  </td>
+                </tr>
+              );
+            }
+          })}
         </tbody>
-        {/*end::Table body*/}
+        <Link to="/">
+          <button className="btn btn-danger">Go Back</button>
+        </Link>
       </table>
-
-      <Link to="/">
-        <button className="btn btn-danger">Go Back</button>
-      </Link>
-      {/* </> */}
     </>
   );
 }
