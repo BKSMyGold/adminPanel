2cc1a92f7dfeeadc41491869bf4a7477a953717a
adeptcoder014 <gupta.nischal014@gmail.com>
2022-07-05 19:46:54 +0530

    Unit Screen and Form UI

---
+++ src/components/UnitsForm.js
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+import Dashboard from "../screens/dashboard";
+import { isValidMetalGroup } from "../Validator";
+import { addMetalGroup, updateMetalGroup } from "../apis/MetalGroup";
+import AddUpdateSpinner from "../AddUpdateSpinner";
+//===================================================================================
+const UnitsForm = (props) => {
+  //===================================================================================
+  let location = useLocation();
+  console.log(location.state);
+  let navigate = useNavigate();
+  //===================================================================================
+  const [isUpdate, setIsUpdate] = useState(location?.state ? true : false);
+  const [units, setUnits] = useState(
+    location?.state ?? {
+      unitName: "",
+      conversionFactorToGold:0,
+    }
+  );
+  //===================================================================================
+  return (
+    <div className="d-flex flex-column flex-root">
+      <div className="page d-flex flex-row flex-column-fluid">
+        <div
+          className="wrapper d-flex flex-column flex-row-fluid"
+          id="kt_wrapper"
+        >
+          <Header />
+          {/* < Dashboard /> */}
+          <div
+            id="kt_content_container"
+            class="d-flex flex-column-fluid align-items-start container-xxl"
+          >
+            {/*begin::Post*/}
+            <div class="content flex-row-fluid" id="kt_content">
+              {/*begin::Row*/}
+
+              {/*begin::Tables Widget 13*/}
+              <div class="card mb-5 mb-xl-8">
+                {/*begin::Header*/}
+                <div class="card-header border-0 pt-5">
+                  <h3 class="card-title align-items-start flex-column">
+                    <span class="card-label fw-bolder fs-3 mb-1">
+                      {isUpdate ? "Update Units" : "Add Units"}
+                    </span>
+                    <span class="text-muted mt-1 fw-bold fs-7">
+                      {isUpdate ? "Update Units" : "Add Units"}
+                    </span>
+                  </h3>
+                </div>
+                {/*end::Header*/}
+                {/*begin::Body*/}
+                <div class="card-body py-3">
+                  {/*begin::Table container*/}
+                  <div class="table-responsive">
+                    <form>
+                      <div>
+                        <label class="d-flex align-items-center fs-5 fw-bold mb-2">
+                          <span class="required">Unit Name</span>
+                          <i
+                            class="fas fa-exclamation-circle ms-2 fs-7"
+                            data-bs-toggle="tooltip"
+                            title="Enter the Name of the Units"
+                          ></i>
+                        </label>
+                        <input
+                          type="text"
+                          name="unitName"
+                          className="form-control form-control-lg form-control-solid"
+                          placeholder="Enter Unit Name"
+                          onChange={(e) =>
+                            setUnits({
+                              ...units,
+                              unitName: e.target.value,
+                            })
+                          }
+                            value={units.unitName}                          
+                        />
+                      </div>
+                      <div>
+                        <label class="d-flex align-items-center fs-5 fw-bold mb-2">
+                          <span class="required">Conversion Factor</span>
+                          <i
+                            class="fas fa-exclamation-circle ms-2 fs-7"
+                            data-bs-toggle="tooltip"
+                            title="Conversion Factor for the Gold Metal"
+                          ></i>
+                        </label>
+                        <input
+                          type="number"
+                          name="converionFactor"
+                          multiple
+                          className="form-control form-control-lg form-control-solid"
+                          placeholder="Enter the conversion factor"
+                          value={units.conversionFactorToGold}    
+                          onChange={(e) => {
+                            setUnits({
+                              ...units,
+                              conversionFactorToGold: e.target.value,
+                            });
+                          }}
+                        />
+                      </div>
+                      <div>
+                        <br />
+                        <button
+                          className="btn btn-lg btn-primary"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            isUpdate
+                              ? console.log({ ...units })
+                              : console.log({ ...units })
+                          }}
+                        >
+                          {isUpdate ? "Update Units" : "Add Units"}
+                        </button>
+                      </div>
+                      {/* <AddUpdateSpinner
+                        update={isUpdate ? true : false}
+                        collection={metalGroup}
+                        adding={addMetalGroup}
+                        updating={updateMetalGroup}
+                        url={"/master/product-data/metal-groups/"}
+                      /> */}
+                    </form>
+                  </div>
+                  {/*end::Table container*/}
+                </div>
+                {/*begin::Body*/}
+              </div>
+              {/*end::Tables Widget 13*/}
+            </div>
+            {/*end::Post*/}
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UnitsForm;
