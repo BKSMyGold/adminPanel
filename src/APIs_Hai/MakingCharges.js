fb1ec16f8d8380e2b56a30202127bcf8dc2bb339
Nischal <gupta.nischal014@gmail.com>
2022-07-09 11:33:01 +0530

    Integration of MakingCharges APIs

--- src/APIs_Hai/MakingCharges.js
+++ src/APIs_Hai/MakingCharges.js
@@ -2,17 +2,34 @@ import axios from "axios";
 import { ADMIN_API } from "../Constants";
 //=====================================================
 
-export const addMakingCharges = async (makingCharges) => {                           // POST
-    await axios.post(`${ADMIN_API}/admin/makingcharge/create/`,{...makingCharges});
+export const addMakingCharges = async (makingCharges) => {
+  let { supplierName,
+    variety,
+    item,
+    metalId,
+    fromWeight,
+    toWeight,
+    rateType,
+    rate } = makingCharges 
+    
+    let data = { supplierName,
+      variety,
+      item,
+      metalId,
+      fromWeight:Number(fromWeight),
+      toWeight: Number(toWeight),
+      rateType,
+      rate: Number(rate) }
+  await axios.post(`${ADMIN_API}/admin/makingcharge/create/`, data);
 };
 
 export const getMakingCharges = async () => {
   return await axios.post(`${ADMIN_API}/admin/makingcharge/list`);   // GET
-  
+
 };
 
 export const updateMakingCharges = async (makingCharges) => {
-  return await axios.put(`${ADMIN_API}/admin/makingcharge/update/${makingCharges.id}`, {...makingCharges}); // PUT
+  return await axios.put(`${ADMIN_API}/admin/makingcharge/update/${makingCharges.id}`, { ...makingCharges }); // PUT
 };
 
 export const deleteMakingCharges = async (id) => {
