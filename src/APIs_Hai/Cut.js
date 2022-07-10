cd252257f8fc6726289dff91ee45c41f9b8d048b
Nischal <gupta.nischal014@gmail.com>
2022-07-10 13:41:20 +0530

    Cut APIs Integration

--- src/APIs_Hai/Cut.js
+++ src/APIs_Hai/Cut.js
@@ -2,21 +2,20 @@ import axios from "axios";
 import { ADMIN_API } from "../Constants";
 //=====================================================
 
-export const addCutShape = async (cutShape) => {                           // POST
-    await axios.post(`${ADMIN_API}/admin/shape/create/`,{...cutShape});
+export const addCut = async (cut) => {
+    await axios.post(`${ADMIN_API}/admin/cut/create/`, {...cut});
 };
 
-export const getCutShape = async () => {
-  return await axios.post(`${ADMIN_API}/admin/shape/list`);   // GET
-  
+export const getCut = async () => {
+    return await axios.post(`${ADMIN_API}/admin/cut/list`);   // GET
+
 };
 
-export const updateCutShape = async (cutShape) => {
- 
-  return await axios.put(`${ADMIN_API}/admin/shape/update/${cutShape.id}`, {...cutShape}); // PUT
+export const updateCut = async (cut) => {
+    return await axios.put(`${ADMIN_API}/admin/cut/update/${cut.id}`, {...cut}); // PUT
 };
 
-export const deleteCutShape = async (id) => {
-  await axios.delete(`${ADMIN_API}/admin/shape/delete/${id}`); // DELETE
-  window.location.reload(false);
+export const deleteCut = async (id) => {
+    await axios.delete(`${ADMIN_API}/admin/cut/delete/${id}`); // DELETE
+    window.location.reload(false);
 };
---
+++ src/components/CutForm.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+import Dashboard from "../screens/dashboard";
+import { isValidMetalGroup } from "../Validator";
+import AddUpdateSpinner from "../AddUpdateSpinner";
+import {addCut,updateCut} from "../APIs_Hai/Cut"
+//===================================================================================
+const ColourForm = (props) => {
+  //===================================================================================
+  let location = useLocation();
+  console.log(location.state);
+  let navigate = useNavigate();
+  //===================================================================================
+  const [isUpdate, setIsUpdate] = useState(location?.state ? true : false);
+  const [cut, setCut] = useState(
+    location?.state ?? {
+        name: "",
+      
+    }
+  );
+  //===================================================================================
+  return (
+    <div className="d-flex flex-column flex-root">
+      <div className="page d-flex flex-row flex-column-fluid">
+        <div
+          className="wrapper d-flex flex-column flex-row-fluid"
+          id="kt_wrapper"
+        >
+          <Header />
+          {/* < Dashboard /> */}
+          <div
+            id="kt_content_container"
+            class="d-flex flex-column-fluid align-items-start container-xxl"
+          >
+            {/*begin::Post*/}
+            <div class="content flex-row-fluid" id="kt_content">
+              {/*begin::Row*/}
+
+              {/*begin::Tables Widget 13*/}
+              <div class="card mb-5 mb-xl-8">
+                {/*begin::Header*/}
+                <div class="card-header border-0 pt-5">
+                  <h3 class="card-title align-items-start flex-column">
+                    <span class="card-label fw-bolder fs-3 mb-1">
+                      {isUpdate ? "Update Colour" : "Add Colour"}
+                    </span>
+                    <span class="text-muted mt-1 fw-bold fs-7">
+                      {isUpdate ? "Update Cut" : "Add Cut"}
+                    </span>
+                  </h3>
+                </div>
+                {/*end::Header*/}
+                {/*begin::Body*/}
+                <div class="card-body py-3">
+                  {/*begin::Table container*/}
+                  <div class="table-responsive">
+                    <form>
+                      <div>
+                        <label class="d-flex align-items-center fs-5 fw-bold mb-2">
+                          <span class="required">Cut Name</span>
+                          <i
+                            class="fas fa-exclamation-circle ms-2 fs-7"
+                            data-bs-toggle="tooltip"
+                            title="Enter the Name of the Colour"
+                          ></i>
+                        </label>
+                        <input
+                          type="text"
+                          name="cut"
+                          className="form-control form-control-lg form-control-solid"
+                          placeholder="Enter Colour Name"
+                          onChange={(e) =>
+                            setCut({
+                              ...cut,
+                              name: e.target.value,
+                            })
+                          }
+                            value={cut.name}                          
+                        />
+                      </div>
+                      
+
+                      {/* <div>
+                        <br />
+                        <button
+                          className="btn btn-lg btn-primary"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            isUpdate
+                              ? console.log({ ...colour })
+                              : console.log({ ...colour })
+                          }}
+                        >
+                          {isUpdate ? "Update Colour" : "Add Colour"}
+                        </button>
+                      </div> */}
+                      <AddUpdateSpinner
+                        update={isUpdate ? true : false}
+                        collection={cut}
+                        adding={addCut}
+                        updating={updateCut}
+                        url={"/master/product-data/cut/"}
+                      />
+                    </form>
+                  </div>
+                  {/*end::Table container*/}
+                </div>
+                {/*begin::Body*/}
+              </div>
+              {/*end::Tables Widget 13*/}
+            </div>
+            {/*end::Post*/}
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ColourForm;
