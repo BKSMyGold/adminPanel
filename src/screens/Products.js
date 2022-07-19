e127a9ceb73eea227f57fdfd6d1e4c56385980ea
Nischal <gupta.nischal014@gmail.com>
2022-07-19 10:30:26 +0530

    Loader added

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -6,17 +6,21 @@ import axios from "axios";
 import { getProduct,deleteProduct } from "../APIs_Hai/Product";
 import {Link} from "react-router-dom"
 import DeleteSpinner from "../delete";
-
+import LinearProgress from "@mui/material/LinearProgress";
+import Box from "@mui/material/Box"
 //===========================================================================================================
 const ItemDetails = (props) => {
   //===========================================================================================================
   const [product, setProduct] = useState([]);
   const [userPermissions, setUserPermissions] = useState(new Set());
+  const [loader, setLoader] = useState(false);
 
   //===========================================================================================================
   useEffect(() => {
-    getProduct().then((res) => setProduct(res.data.data.data));
-  }, []);
+    setLoader(true);
+    getProduct().then((res) => {
+      return setProduct(res.data.data.data), setLoader(false);
+    });  }, []);
   console.log(product);
   //===========================================================================================================
   //===========================================================================================================
@@ -108,100 +112,107 @@ const ItemDetails = (props) => {
                       </thead>
 
                       <tbody>
-                        {product?.map((itemDetail) => (
-                          <tr class="fw-bolder text-center">
-                            <td>{itemDetail.id}</td>
-                            <td>{itemDetail.collectionName}</td>
-                            <td>
-                              {itemDetail.category.map((x) => {
-                                return <td>{x}</td>;
+                        {loader ? (
+                           <Box  sx={{ width: '100vw'}}>
+                           <LinearProgress />
+                         </Box>
+                        ):(
+                          product?.map((itemDetail) => (
+                            <tr class="fw-bolder text-center">
+                              <td>{itemDetail.id}</td>
+                              <td>{itemDetail.collectionName}</td>
+                              <td>
+                                {itemDetail.category.map((x) => {
+                                  return <td>{x}</td>;
+                                })}
+                              </td>
+                              <td>
+                                {itemDetail.variety.map((x) => {
+                                  return <td>{x}</td>;
+                                })}
+                              </td>
+                              <td>{itemDetail.item}</td>
+                              <td>{itemDetail.grossWeight}</td>
+                              <td>{itemDetail.sku}</td>
+                              <td>{itemDetail.hli}</td>
+                              <td>{itemDetail.width}</td>
+                              <td>{itemDetail.height}</td>
+                              <td>
+                              {itemDetail.styleComposition.map((x) => {
+                                return (
+                                  <>
+                                    <td>{x.clarity}</td>
+                                    <td>{x.colour}</td>
+                                    <td>{x.cut}</td>
+                                    <td>{x.id}</td>
+                                    <td>{x.shape}</td>
+                                    <td>{x.style}</td>
+                                    <td>{x.weight}</td>
+                                  </>
+                                );
                               })}
-                            </td>
-                            <td>
-                              {itemDetail.variety.map((x) => {
-                                return <td>{x}</td>;
+                              </td>
+                              <td>
+                              {itemDetail.purityComposition.map((x) => {
+                                return (
+                                  <>
+                                    <td>{x.metalGroup}</td>
+                                    <td>{x.weight}gm</td>
+                                  </>
+                                );
                               })}
-                            </td>
-                            <td>{itemDetail.item}</td>
-                            <td>{itemDetail.grossWeight}</td>
-                            <td>{itemDetail.sku}</td>
-                            <td>{itemDetail.hli}</td>
-                            <td>{itemDetail.width}</td>
-                            <td>{itemDetail.height}</td>
-                            <td>
-                            {itemDetail.styleComposition.map((x) => {
-                              return (
-                                <>
-                                  <td>{x.clarity}</td>
-                                  <td>{x.colour}</td>
-                                  <td>{x.cut}</td>
-                                  <td>{x.id}</td>
-                                  <td>{x.shape}</td>
-                                  <td>{x.style}</td>
-                                  <td>{x.weight}</td>
-                                </>
-                              );
-                            })}
-                            </td>
-                            <td>
-                            {itemDetail.purityComposition.map((x) => {
-                              return (
-                                <>
-                                  <td>{x.metalGroup}</td>
-                                  <td>{x.weight}gm</td>
-                                </>
-                              );
-                            })}
-                            </td>
-                            {/* <td>
-                              {itemDetail.images.map((x) => {
-                                return <img src={x} />;
-                              })}
-                            </td> */}
-                            <td>
-                              <a href={itemDetail.video}>video</a>
-                            </td>
-
-                            <td>
-                              <a
-                                href="#"
-                                class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1"
-                              >
-                                 <Link
-                                  to={"/master/product-data/products/edit"}
-                                  state={itemDetail}
+                              </td>
+                              {/* <td>
+                                {itemDetail.images.map((x) => {
+                                  return <img src={x} />;
+                                })}
+                              </td> */}
+                              <td>
+                                <a href={itemDetail.video}>video</a>
+                              </td>
+  
+                              <td>
+                                <a
+                                  href="#"
+                                  class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1"
                                 >
-                                <span class="svg-icon svg-icon-3">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
+                                   <Link
+                                    to={"/master/product-data/products/edit"}
+                                    state={itemDetail}
                                   >
-                                    <path
-                                      opacity="0.3"
-                                      d="M21.4 8.35303L19.241 10.511L13.485 4.755L15.643 2.59595C16.0248 2.21423 16.5426 1.99988 17.0825 1.99988C17.6224 1.99988 18.1402 2.21423 18.522 2.59595L21.4 5.474C21.7817 5.85581 21.9962 6.37355 21.9962 6.91345C21.9962 7.45335 21.7817 7.97122 21.4 8.35303ZM3.68699 21.932L9.88699 19.865L4.13099 14.109L2.06399 20.309C1.98815 20.5354 1.97703 20.7787 2.03189 21.0111C2.08674 21.2436 2.2054 21.4561 2.37449 21.6248C2.54359 21.7934 2.75641 21.9115 2.989 21.9658C3.22158 22.0201 3.4647 22.0084 3.69099 21.932H3.68699Z"
-                                      fill="black"
-                                    />
-                                    <path
-                                      d="M5.574 21.3L3.692 21.928C3.46591 22.0032 3.22334 22.0141 2.99144 21.9594C2.75954 21.9046 2.54744 21.7864 2.3789 21.6179C2.21036 21.4495 2.09202 21.2375 2.03711 21.0056C1.9822 20.7737 1.99289 20.5312 2.06799 20.3051L2.696 18.422L5.574 21.3ZM4.13499 14.105L9.891 19.861L19.245 10.507L13.489 4.75098L4.13499 14.105Z"
-                                      fill="black"
-                                    />
-                                  </svg>
-                                </span>
-                                {/*end::Svg Icon*/}
-                                </Link>
-                              </a>
-                              {userPermissions.has("delete_offer") ? (
-                                <DeleteSpinner
-                                  collection={itemDetail}
-                                  deleting={deleteProduct}
-                                  url={"/master/product-data/products/"}
-                                />
-                              ) : null}
-                            </td>
-                          </tr>
+                                  <span class="svg-icon svg-icon-3">
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      width="24"
+                                      height="24"
+                                      viewBox="0 0 24 24"
+                                      fill="none"
+                                    >
+                                      <path
+                                        opacity="0.3"
+                                        d="M21.4 8.35303L19.241 10.511L13.485 4.755L15.643 2.59595C16.0248 2.21423 16.5426 1.99988 17.0825 1.99988C17.6224 1.99988 18.1402 2.21423 18.522 2.59595L21.4 5.474C21.7817 5.85581 21.9962 6.37355 21.9962 6.91345C21.9962 7.45335 21.7817 7.97122 21.4 8.35303ZM3.68699 21.932L9.88699 19.865L4.13099 14.109L2.06399 20.309C1.98815 20.5354 1.97703 20.7787 2.03189 21.0111C2.08674 21.2436 2.2054 21.4561 2.37449 21.6248C2.54359 21.7934 2.75641 21.9115 2.989 21.9658C3.22158 22.0201 3.4647 22.0084 3.69099 21.932H3.68699Z"
+                                        fill="black"
+                                      />
+                                      <path
+                                        d="M5.574 21.3L3.692 21.928C3.46591 22.0032 3.22334 22.0141 2.99144 21.9594C2.75954 21.9046 2.54744 21.7864 2.3789 21.6179C2.21036 21.4495 2.09202 21.2375 2.03711 21.0056C1.9822 20.7737 1.99289 20.5312 2.06799 20.3051L2.696 18.422L5.574 21.3ZM4.13499 14.105L9.891 19.861L19.245 10.507L13.489 4.75098L4.13499 14.105Z"
+                                        fill="black"
+                                      />
+                                    </svg>
+                                  </span>
+                                  {/*end::Svg Icon*/}
+                                  </Link>
+                                </a>
+                                {userPermissions.has("delete_offer") ? (
+                                  <DeleteSpinner
+                                    collection={itemDetail}
+                                    deleting={deleteProduct}
+                                    url={"/master/product-data/products/"}
+                                  />
+                                ) : null}
+                              </td>
+                            </tr>
+                        )
+                      
                         ))}
                       </tbody>
                       {/*end::Table body*/}
